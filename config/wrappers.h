2ba9ea7db01eb9cd8668a62e4d23390db6203070
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2021-11-11 19:37:45 +0100

    adds HDD

--- config/wrappers.h
+++ config/wrappers.h
@@ -1,9 +1,19 @@
 #define ___dusty_L1___       &none      &kp K       &kp L        &kp D        &kp COMMA
 #define ___dusty_L2___       &hm LGUI R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT P
 #define ___dusty_L3___       &kp X      &kp G       &kp M        &kp F        &kp DOT
-#define ___dusty_L4___       &lt BLE_LAYER ESC &lt NAV_LAYER SPACE
+#define ___dusty_L4___                              &lt BLE_LAYER ESC &lt NAV_LAYER SPACE
 
 #define ___dusty_R1___       &kp Q      &kp W        &kp H        &kp C       &kp Y
 #define ___dusty_R2___       &hm RALT Z &hm LSHFT A  &hm LCTRL U  &hm LALT O  &hm LGUI I
 #define ___dusty_R3___       &kp QMARK  &kp V        &kp B        &kp J       &kp SQT
 #define ___dusty_R4___       &lt NUM_LAYER E  &kp BSPC
+
+#define ___HDD_L1___    &kp J      &kp G       &kp M        &kp P        &kp V
+#define ___HDD_L2___    &hm LGUI R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT W
+#define ___HDD_L3___    &kp X      &kp C       &kp L        &kp D        &kp B
+#define ___HDD_L4___    &lt BLE_LAYER ESC  &lt NAV_LAYER E
+
+#define ___HDD_R1___    & MINUS        &kp DOT      &kp SQT      &kp DQT     &kp FSLH
+#define ___HDD_R2___    &hm RALT COMMA &hm LSHFT H  &hm LCTRL A  &hm LALT O  &hm LGUI I
+#define ___HDD_R3___    &kp Z          &kp F        &kp U        &kp K       &kp Y
+#define ___HDD_R4___    &lt NUM_LAYER SPC  &none
