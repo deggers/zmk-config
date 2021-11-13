eb2eea55378b1b79fac8885967eacd3eaa5c47c5
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2021-11-13 10:14:30 +0100

    swap E and SPC on HDD

--- config/wrappers.h
+++ config/wrappers.h
@@ -11,9 +11,9 @@
 #define ___HDD_L1___    &kp J      &kp G       &kp M        &kp P        &kp V
 #define ___HDD_L2___    &hm LGUI R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT W
 #define ___HDD_L3___    &kp X      &kp C       &kp L        &kp D        &kp B
-#define ___HDD_L4___    &lt BLE_LAYER ESC  &lt NAV_LAYER SPC
+#define ___HDD_L4___    &lt BLE_LAYER ESC  &lt NAV_LAYER E
 
 #define ___HDD_R1___    &kp MINUS        &kp DOT      &kp SQT      &kp DQT     &kp FSLH
 #define ___HDD_R2___    &hm RALT COMMA   &hm LSHFT H  &hm LCTRL A  &hm LALT O  &hm LGUI I
 #define ___HDD_R3___    &kp Z            &kp F        &kp U        &kp K       &kp Y
-#define ___HDD_R4___    &lt NUM_LAYER E  &kp BSPC
+#define ___HDD_R4___    &lt NUM_LAYER SPC  &kp BSPC
