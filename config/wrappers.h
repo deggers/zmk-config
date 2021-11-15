4750f4ac2d86ca0cc4c5f3c3dd815b092881ce65
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2021-11-15 23:31:36 +0100

    FIX changed layers .. uargh.

--- config/wrappers.h
+++ config/wrappers.h
@@ -1,12 +1,12 @@
 #define ___dusty_L1___       &none      &kp K       &kp L        &kp D        &kp COMMA
 #define ___dusty_L2___       &hm LGUI R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT P
 #define ___dusty_L3___       &kp X      &kp G       &kp M        &kp F        &kp DOT
-#define ___dusty_L4___                              &lt BLE_LAYER ESC &lt NAV_LAYER SPACE
+#define ___dusty_L4___                              &lt BLE_LAYER ESC &kp SPACE
 
 #define ___dusty_R1___       &kp Q      &kp W        &kp H        &kp C       &kp Y
 #define ___dusty_R2___       &hm RALT Z &hm LSHFT A  &hm LCTRL U  &hm LALT O  &hm LGUI I
 #define ___dusty_R3___       &kp QMARK  &kp V        &kp B        &kp J       &kp SQT
-#define ___dusty_R4___       &lt NUM_LAYER E  &kp BSPC
+#define ___dusty_R4___       &kp E  &kp BSPC
 
 #define ___HDD_L1___    &kp J      &kp G       &kp M        &kp P        &kp V
 #define ___HDD_L2___    &hm LGUI R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT W
