ba8f42146cd136ebad285a2547cb373404b8b398
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2021-11-24 21:11:31 +0100

    add some combos to fifi and back to hold-layer

--- config/wrappers.h
+++ config/wrappers.h
@@ -1,25 +1,32 @@
-#define ___dusty_L1___       &none      &kp K       &kp L        &kp D        &kp COMMA
-#define ___dusty_L2___       &hm LGUI R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT P
-#define ___dusty_L3___       &kp X      &kp G       &kp M        &kp F        &kp DOT
-#define ___dusty_L4___                              &lt BLE_LAYER ESC &kp SPACE
+#define ___dusty_L1___       &none   &kp K  &kp L  &kp D  &kp COMMA
+#define ___dusty_L2___       &kp R   &kp S  &kp N  &kp T  &kp P
+#define ___dusty_L3___       &kp X   &kp G  &kp M  &kp F  &kp DOT
 
-#define ___dusty_R1___       &kp Q      &kp W        &kp H        &kp C       &kp Y
-#define ___dusty_R2___       &hm RALT Z &hm LSHFT A  &hm LCTRL U  &hm LALT O  &hm LGUI I
-#define ___dusty_R3___       &kp QMARK  &kp V        &kp B        &kp J       &kp SQT
-#define ___dusty_R4___       &kp E  &kp BSPC
+#define ___dusty_R1___       &kp Q      &kp W  &kp H  &kp C  &kp Y
+#define ___dusty_R2___       &kp Z      &kp A  &kp U  &kp O  &kp I
+#define ___dusty_R3___       &kp QMARK  &kp V  &kp B  &kp J  &kp SQT
 
 #define ___HDD_L1___    &kp J      &kp G       &kp M        &kp P        &kp V
 #define ___HDD_L2___    &hm LGUI R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT W
 #define ___HDD_L3___    &kp X      &kp C       &kp L        &kp D        &kp B
-#define ___HDD_L4___    &lt BLE_LAYER ESC  &lt NAV_LAYER E
 
 #define ___HDD_R1___    &kp MINUS        &kp DOT      &kp SQT      &kp DQT     &kp FSLH
 #define ___HDD_R2___    &hm RALT COMMA   &hm LSHFT H  &hm LCTRL A  &hm LALT O  &hm LGUI I
 #define ___HDD_R3___    &kp Z            &kp F        &kp U        &kp K       &kp Y
-#define ___HDD_R4___    &lt NUM_LAYER SPC  &kp BSPC
 
 #define ___BLE_L1___    &reset       &none        &none        &out OUT_BLE &bt BT_CLR
 #define ___BLE_L2___    &bt BT_SEL 0 &bt BT_SEL 1 &bt BT_SEL 2 &bt BT_SEL 3 &bt BT_SEL 4
 #define ___BLE_L3___    &bootloader  &none        &none        &out OUT_USB &none
 
 #define ___NONE___      &none        &none        &none        &none        &none
+
+// Old Stuff
+#define ___dusty_L1_HRM___       &none      &kp K       &kp L        &kp D        &kp COMMA
+#define ___dusty_L2_HRM___       & R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT P
+#define ___dusty_L3_HRM___       &kp X      &kp G       &kp M        &kp F        &kp DOT
+#define ___dusty_L4_HRM___                              &lt BLE_LAYER ESC &kp SPACE
+
+#define ___dusty_R1_HRM___       &kp Q      &kp W        &kp H        &kp C       &kp Y
+#define ___dusty_R2_HRM___       &hm RALT Z &hm LSHFT A  &hm LCTRL U  &hm LALT O  &hm LGUI I
+#define ___dusty_R3_HRM___       &kp QMARK  &kp V        &kp B        &kp J       &kp SQT
+#define ___dusty_R4_HRM___       &kp E  &kp BSPC
