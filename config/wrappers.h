fc41e29038f806b67d2cdd085e454a7ab3b5e83f
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2021-12-22 15:33:18 +0100

    removes deleted HM lalilu

--- config/wrappers.h
+++ config/wrappers.h
@@ -18,14 +18,6 @@
 #define ___APTex_R2___       &mtr RALT deY      &mtr LSHIFT N  &mtr LCTRL A  &mtr LALT I  &mtr LGUI L
 #define ___APTex_R3___       &kp deZ &kp P  &kp deSQT  &kp COMMA  &kp DOT
 
-#define ___HDD_L1___    &kp J      &kp G       &kp M        &kp P        &kp V
-#define ___HDD_L2___    &hm LGUI R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT W
-#define ___HDD_L3___    &kp X      &kp C       &kp L        &kp D        &kp B
-
-#define ___HDD_R1___    &kp MINUS        &kp DOT      &kp deSQT      &kp deDQT     &kp deSLASH
-#define ___HDD_R2___    &hm RALT COMMA   &hm LSHFT H  &hm LCTRL A  &hm LALT O  &hm LGUI I
-#define ___HDD_R3___    &kp deZ          &kp F        &kp U        &kp K       &kp deY
-
 #define ___APTexD_L1___    &kp X      &kp W       &kp C        &kp K        &kp deZ
 #define ___APTexD_L2___    &kp R      &kp S       &kp T        &kp H        &kp M
 #define ___APTexD_L3___    &kp J      &kp V       &kp G        &kp D        &kp deY
@@ -51,11 +43,11 @@
 
 // Old Stuff
 #define ___dusty_L1_HRM___       &none      &kp K       &kp L        &kp D        &kp COMMA
-#define ___dusty_L2_HRM___       &kp R &hm LALT S  &hm LCTRL N  &hm LSHFT T  &hm RALT P
+#define ___dusty_L2_HRM___       &kp R      &kp S       &kp N        &kp T        &kp P
 #define ___dusty_L3_HRM___       &kp X      &kp G       &kp M        &kp F        &kp DOT
 #define ___dusty_L4_HRM___                              &lt BLE_LAYER ESC &kp SPACE
 
 #define ___dusty_R1_HRM___       &kp Q      &kp W        &kp H        &kp C       &kp Y
-#define ___dusty_R2_HRM___       &hm RALT Z &hm LSHFT A  &hm LCTRL U  &hm LALT O  &hm LGUI I
+#define ___dusty_R2_HRM___       &kp Z      &kp A        &kp U        &kp O       &kp I
 #define ___dusty_R3_HRM___       &kp QMARK  &kp V        &kp B        &kp J       &kp SQT
 #define ___dusty_R4_HRM___       &kp E  &kp BSPC
