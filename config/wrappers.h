6241e74793aaccea314ad0c35b3c8328baf6f68e
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2022-01-16 12:21:17 +0100

    add UML_Layer

--- config/wrappers.h
+++ config/wrappers.h
@@ -2,20 +2,6 @@
 
 // layouts 
 
-/* dusty refined 
-	x f l d v  - u o c k
-	r s n t b  , i a m h
-	' w j p q  ? . z g y e
-*/ 
-
-#define ___dusty_L1___       &kp X     &kp F  &kp L  &kp D  &kp V
-#define ___dusty_L2___       &kp R     &kp S  &kp N  &kp T  &kp B
-#define ___dusty_L3___       &kp deSQT &kp W  &kp J  &kp P  &kp Q
-
-#define ___dusty_R1___       &none          &kp U    &kp O    &kp C  &kp K
-#define ___dusty_R2___       &kp COMMA      &kp I    &kp A    &kp M  &kp H
-#define ___dusty_R3___       &kp deQUESTION &kp DOT  &kp deZ  &kp G  &kp deY
-
 /*  NAIL 
   ' f c k ?  - b o u z
   r s t h d  q n a i l
@@ -29,33 +15,6 @@
 #define ___nail_R1___    &kp deGRAVE  &kp B         &kp O       &kp U        &kp deZ
 #define ___nail_R2___    &kp Q        &hm LSHIFT N  &hm LALT A  &hm LCTRL I  &hm LGUI L
 #define ___nail_R3___    &kp W        &kp P         &kp DOT     &kp COMMA    &kp J
-
-
-#define ___APTex_L1___       &kp deSLASH &kp W  &kp G  &kp D  &kp B
-#define ___APTex_L2___       &mtl LGUI R   &mtl LALT S  &mtl LCTRL T  &mtl LSHFT H  &mtl RALT V
-#define ___APTex_L3___       &kp X   &kp C  &kp M  &kp K  &kp Q
-
-#define ___APTex_R1___       &kp J      &kp F  &kp O  &kp U  &kp deQUESTION
-#define ___APTex_R2___       &mtr RALT deY      &mtr LSHIFT N  &mtr LCTRL A  &mtr LALT I  &mtr LGUI L
-#define ___APTex_R3___       &kp deZ &kp P  &kp deSQT  &kp COMMA  &kp DOT
-
-#define ___APTexD_L1___    &kp X      &kp W       &kp C        &kp K        &kp deZ
-#define ___APTexD_L2___    &kp R      &kp S       &kp T        &kp H        &kp M
-#define ___APTexD_L3___    &kp J      &kp V       &kp G        &kp D        &kp deY
-
-#define ___APTexD_R1___    &kp deSQT  &kp F       &kp O        &kp U        &kp Q
-#define ___APTexD_R2___    &kp B      &kp N       &kp A        &kp I        &kp L
-#define ___APTexD_R3___    &kp deSEMI &kp P       &kp deSLASH  &kp COMMA    &kp DOT
-
-
-
-#define ___louvre_L1___    &kp deZ    &kp F       &kp M        &kp D        &kp deY
-#define ___louvre_L2___    &kp R      &kp S       &kp T        &kp H        &kp P
-#define ___louvre_L3___    &kp X      &kp W       &kp V        &kp K        &kp B
-
-#define ___louvre_R1___    &kp deSEMI  &kp L       &kp U        &kp O        &kp G
-#define ___louvre_R2___    &kp deSQT   &kp N       &kp I        &kp A        &kp C
-#define ___louvre_R3___    &kp deSLASH &kp J       &kp DOT      &kp COMMA    &kp Q
 
 // other stuff than layouts 
 
